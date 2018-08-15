f0c6be8c9d19c19e779176e98a9df0bf653671fc
LeksorHayabusa <alexmotorniyg@gmail.com>
2018-08-15 23:37:01 +0200

    added infinite scroll

--- src/itemlist.js
+++ src/itemlist.js
@@ -3,6 +3,21 @@ import { Link } from 'react-router-dom'
 import ItemThumbnail from './ItemThumbnail'
 
 class ItemList extends Component {
+
+	handleScroll(e) {
+		const scrolled = window.innerHeight + window.scrollY,
+		preBottom = document.body.offsetHeight - 500,
+		items = this.props.mainState.items;
+		if(scrolled >= preBottom && items.length) this.props.uploadNextItems()
+	}
+
+	componentDidMount = () => {
+		window.addEventListener('scroll', (e) => this.handleScroll(e))
+	}
+	
+	componentWillUnmount = () => {
+		window.removeEventListener('scroll', this.handleScroll)
+	}
 	
 	render() {
 		const { 
